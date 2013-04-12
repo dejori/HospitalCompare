39e1a94d605ac2664cfd6ecabe6b676b76b76bbd
dejori <dejori@gmail.com>
2013-04-11 17:29:24 -0700

    added code to create boxplots

--- hospital_compare/analyze_hcahps_survey.py
+++ hospital_compare/analyze_hcahps_survey.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import math
-import numpy as np
 import os
+import json
 from hospital_compare.HCAHPSController import HCAHPSController
 from hospital_compare.HospitalInformationController import HospitalInformationController
+from numpy.ma.core import get_data
 
 hcaphs_controller = HCAHPSController()
 hospital_controller = HospitalInformationController()
@@ -12,84 +13,52 @@ RESULT_DIR = os.path.join(os.path.dirname(__file__), "..", 'results') # change t
 if not os.path.isdir(RESULT_DIR):
     os.makedirs(RESULT_DIR)
 
-def save_histogram(positive_questions, title, xlabel,filename):
+def get_data(positive_questions):
     positives = []
     for hospitalnumber in hospital_controller.get_providernumbers():
         positive = sum([hcaphs_controller.get_answerpercentage(hospitalnumber, question) for question in positive_questions])
         if not math.isnan(positive):
             positives.append(positive)
-    mean = np.mean(positives)
-    hist, bins = np.histogram(positives, bins=15)
+    return positives
+    
+    
+def save_boxplots(positives, xlabels, title,filename):
+   
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.bar((bins[:-1] + bins[1:]) / 2, hist, align='center', width=(bins[1] - bins[0]), color='#333333', edgecolor='#333333')
-    ax.axvline(mean, color='#FF5C10')
-    ax.text(mean, 10, ' $\mu=%0.1f$' % mean,color='#FF5C10')
-    plt.ylabel('# of Hospitals')
-    plt.xlabel(xlabel)
+    ax.boxplot(positives,0,'')
+    
     plt.title(title)
-    plt.xlim([0, 100])
-    ax.set_xticks(range(0, 100 + 1, 20))
-    ax.set_xticklabels(['', '20%', '40%', '60%', '80%', '100%'])
-    plt.savefig(filename)
+    plt.ylabel('Percentage of patients who gave a positive answer')
+    plt.ylim([0, 110])
+    ax.set_yticks(range(0, 100 + 1, 20))
+    ax.set_yticklabels(['', '20%', '40%', '60%', '80%', '100%'])
+    for idx, xlabel in enumerate(xlabels):
+        ax.text(idx+1, 2, xlabel, rotation = 90, fontsize=8, va='bottom')
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    plt.savefig(os.path.join(RESULT_DIR,filename))
 
 
-save_histogram(['Percent of patients who reported YES, they would probably recommend the hospital.','Patients who reported YES, they would definitely recommend the hospital.'],
-                  'Would you recommend this hospital to your friends and family?',
-                  'Patients who answered with \'YES\'',
-                  os.path.join(RESULT_DIR,"recommend.png")
-                  )
+questions_file = open(os.path.join(os.path.dirname(__file__), '..', 'data','hcahps_questions.json'))
+question_groups = json.load(questions_file)
+questions_file.close()
 
-save_histogram(['Patients who gave their hospital a rating of 9 or 10 on a scale from 0 (lowest) to 10 (highest).', 'Percent of patients who gave their hospital a rating of 7 or 8 on a scale from 0 (lowest) to 10 (highest).'],
-                  'What number would you use to rate this hospital during your stay?\n(0-10, worst-best) ',
-                  'Patients who gave a rating of 7 or higher',
-                  os.path.join(RESULT_DIR,"rating.png")
-                  )
-
-save_histogram(['Patients who reported that their doctors Always" communicated well."', 'Percent of patients who reported that their doctors Usually" communicated well."'],
-                  'Did doctors communicated well?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                   os.path.join(RESULT_DIR,"communication.png")
-                   )
-
-save_histogram(['Patients who reported that their nurses Always" communicated well."', 'Percent of patients who reported that their nurses Usually" communicated well."'],
-                  'Did doctors communicated well?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                   os.path.join(RESULT_DIR,"communication_nurses.png")
-                   )
-
-save_histogram(['Patients who reported that staff Always" explained about medicines before giving it to them."', 'Percent of patients who reported that staff Usually" explained about medicines before giving it to them."'],
-                  'Before giving you any new medicine,\nhow often did hospital staff tell you what the medicine was for?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                  os.path.join(RESULT_DIR,"medication.png")
-                  )
-
-save_histogram(['Percent of patients who reported that they Usually" received help as soon as they wanted."', 'Patients who reported that they Always" received help as soon as they wanted."'],
-                  'After you pressed the call button,\nhow often did you get help as soon as you wanted it?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                  os.path.join(RESULT_DIR,"help.png")
-                  )
-
-save_histogram(['Percent of patients who reported that their pain was Usually" well controlled."', 'Patients who reported that their pain was Always" well controlled."'],
-                  'How often was your pain well controlled?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                  os.path.join(RESULT_DIR,"pain.png")
-                  )
-
-save_histogram(['Percent of patients who reported that their room and bathroom were Usually" clean."', 'Patients who reported that their room and bathroom were Always" clean."'],
-                  "How often were your room and bathroom kept clean",
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                  os.path.join(RESULT_DIR,"clean.png")
-                  )
-
-save_histogram(['Patients who reported that the area around their room was Always" quiet at night."', 'Percent of patients who reported that the area around their room was Usually" quiet at night."'],
-                  'How often was the area around your room quiet at night?',
-                  'Patients who answered with \'Always\' or \'Usually\'',
-                  os.path.join(RESULT_DIR,"quiet.png")
-                  )
-
-save_histogram(['Patients at each hospital who reported that YES, they were given information about what to do during their recovery at home.'],
-                  "Did staff give you information about what to do\n during your recovery at home?",
-                  'Patients who answered with \'YES\'',
-                  os.path.join(RESULT_DIR,"recovery.png")
-                  )
+all_answer_data=[]
+all_questions=[]
+for question_category in question_groups.keys():
+    print question_category
+    questions = question_groups[question_category];
+    
+    answers_data = []
+    
+    for question in questions.keys():
+        positive_questions = questions.get(question)
+        print "\t"+question +" --> "+ str(positive_questions)
+        answers_data.append(get_data(positive_questions))
+        
+    save_boxplots(answers_data, questions.keys(), question_category, question_category.replace(" ","_")+".png")
+    all_answer_data.extend(answers_data)
+    all_questions.extend(questions.keys())
+    
+save_boxplots(all_answer_data, all_questions, "HCAHPS Results", "all.png")
